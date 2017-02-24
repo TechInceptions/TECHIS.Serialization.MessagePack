1c0cf149babaf78162256b139a3d410b8b77eeba
Arinze <arinze@techinceptions.com>
2017-02-24 14:16:07 -0500

    Add project files.

---
+++ TECHIS.Serialization.MessagePack/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECHIS.Serialization.MessagePack
+{
+    public class Class1
+    {
+    }
+}
