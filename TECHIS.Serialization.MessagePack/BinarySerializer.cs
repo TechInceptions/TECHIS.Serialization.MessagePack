87169273ee1329a590f86d72ae8c081f676dad5c
Arinze <arinze@techinceptions.com>
2017-02-24 14:50:10 -0500

    added messagepack. Implemented IBinarySerialization

---
+++ TECHIS.Serialization.MessagePack/BinarySerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MsgPack.Serialization;
+
+namespace TECHIS.Serialization.MessagePack
+{
+    public class BinarySerializer : TECHIS.Serialization.Absractions.IBinarySerialization
+    {
+        ConcurrentDictionary<string, object> _Serializers = new ConcurrentDictionary<string, object>();
+
+        private MessagePackSerializer<T> GetSerializer<T>()
+        {
+            return (MessagePackSerializer<T>)(_Serializers.GetOrAdd(typeof(T).FullName, (key) => MessagePackSerializer.Get<T>()));
+        }
+        public T Deserialize<T>(Stream stream)
+        {
+            return (GetSerializer<T>()).Unpack(stream);
+        }
+
+        public Task<T> DeserializeAsync<T>(Stream stream)
+        {
+            return (GetSerializer<T>()).UnpackAsync(stream);
+        }
+
+        public void Serialize<T>(T obj, Stream stream)
+        {
+           (GetSerializer<T>()).Pack(stream, obj);
+        }
+
+        public Task SerializeAsync<T>(T obj, Stream stream)
+        {
+           return (GetSerializer<T>()).PackAsync(stream, obj);
+        }
+    }
+}
